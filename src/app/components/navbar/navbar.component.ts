aa37c4bd15698969b75c0a16244053a9453dcf2c
Chanon Ungpansattawong (Non) <chanon.ungpansattawong@thomsonreuters.com>
2022-02-13 10:27:16 +0700

    Clean up

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,16 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
 	selector: 'app-navbar',
 	templateUrl: './navbar.component.html',
 	styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit {
-
-	showFiller = false;
+export class NavbarComponent {
 	constructor() { }
-
-	ngOnInit(): void {
-	}
-
 }
